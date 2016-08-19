c8901c84c1ab648da86ad22c69a2fa1c0b4e27c5
Ivan.Orlov <ivan@morrr.com>
2016-08-19 11:22:19 +0300

    clean up and version bump

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const rename = require("gulp-rename");
 const ts = require('gulp-typescript');
 const concat = require('gulp-concat');
 const tsUniversal = require("./index.js");
-const prettify = require('gulp-jsbeautifier');
 const sourcemaps = require('gulp-sourcemaps');
 const tsProject = ts.createProject('example/tsconfig.json');
 gulp.task('default', function() {
