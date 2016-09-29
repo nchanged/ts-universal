2f29ed7538e7e6fc11acc5d41763196d9d886708
Ivan.Orlov <ivan@morrr.com>
2016-09-29 09:18:35 +0300

    Consuming global fix

--- _footer.js
+++ _footer.js
@@ -2,8 +2,8 @@ var __expose__ = function(n, m, w, c) {
     var e = __resolve__(n);
     var bc;
     if (!$isBackend) { var npm = $__exports__.__npm__ = $__exports__.__npm__ || {}; if (m) { bc = npm[m] } }
-    var cs = c.split(",")
-    for (var ln in __local__) { for (var i = 0; i < cs.length; i++) { if (ln.indexOf(cs[i]) === 0) { __resolve__(ln) } } }
+    var cs = c ? c.split(",") : [];
+    if (cs.length){ for (var ln in __local__) { for (var i = 0; i < cs.length; i++) { if (ln.indexOf(cs[i]) === 0) { __resolve__(ln) } } }}
     for (var k in e) {
         $isBackend || w ? $__exports__[k] = e[k] : null;
         bc ? bc[e] = e[k] : null;
